541aa660509736a1b6605e52b8ed72503e45f480
Pascu-Victor <146840548+Pascu-Victor@users.noreply.github.com>
2025-03-23 20:17:33 +0200

    Simplify window full-screening
    No need for making the window reference a member as it's already a member of Window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System.Windows;
-using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Windows.Graphics;
-using WinRT.Interop;
 
 
 // To learn more about WinUI, the WinUI project structure,
@@ -29,30 +14,16 @@ namespace DrinkDb_Auth
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        private AppWindow m_appWindow;
-
         public MainWindow()
         {
             this.InitializeComponent();
             Title = "DrinkDb - Sign In";
 
-            // Get AppWindow
-            IntPtr hWnd = WindowNative.GetWindowHandle(this);
-            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
-            m_appWindow = AppWindow.GetFromWindowId(wndId);
-
-            // Set window to full screen
-            var displayArea = DisplayArea.GetFromWindowId(wndId, DisplayAreaFallback.Primary);
-            if (displayArea != null)
-            {
-                var size = new SizeInt32
-                {
-                    Width = displayArea.WorkArea.Width,
-                    Height = displayArea.WorkArea.Height
-                };
-                m_appWindow.Resize(size);
-                m_appWindow.Move(new PointInt32(0, 0));
-            }
+            this.AppWindow.Resize(new SizeInt32 {
+                Width = DisplayArea.Primary.WorkArea.Width,
+                Height = DisplayArea.Primary.WorkArea.Height
+            });
+            this.AppWindow.Move(new PointInt32(0, 0));
         }
 
         private void SignInButton_Click(object sender, RoutedEventArgs e)
