cbf3ffe629120c03d172ebf50febebea3b122b5e
VioreliaMotofelea <viorelia.motofelea123@gmail.com>
2025-04-02 13:01:38 +0300

    finish

--- Adapter/PermissionAdapter.cs
+++ Adapter/PermissionAdapter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Configuration;
 using DrinkDb_Auth.Adapter;
 using DrinkDb_Auth.Model;
@@ -16,6 +16,100 @@ namespace DrinkDb_Auth.Adapter
             connectionString = ConfigurationManager.ConnectionStrings["DrinkDbConnection"].ConnectionString;
         }
 
-        
+        public void CreatePermission(Permission permission)
+        {
+            string query = @"INSERT INTO Permissions (permissionName, resource, action)
+                             VALUES (@PermissionName, @Resource, @Action)";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@PermissionName", permission.PermissionName);
+                cmd.Parameters.AddWithValue("@Resource", permission.Resource);
+                cmd.Parameters.AddWithValue("@Action", permission.Action);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdatePermission(Permission permission)
+        {
+            string query = @"UPDATE Permissions 
+                             SET permissionName=@PermissionName, resource=@Resource, action=@Action 
+                             WHERE Id=@Id";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@PermissionName", permission.PermissionName);
+                cmd.Parameters.AddWithValue("@Resource", permission.Resource);
+                cmd.Parameters.AddWithValue("@Action", permission.Action);
+                cmd.Parameters.AddWithValue("@Id", permission.Id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeletePermission(Permission permission)
+        {
+            string query = "DELETE FROM Permissions WHERE Id=@Id";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Id", permission.Id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public Permission GetPermissionById(int id)
+        {
+            Permission permission = null;
+            string query = "SELECT Id, permissionName, resource, action FROM Permissions WHERE Id=@Id";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Id", id);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        permission = new Permission
+                        {
+                            Id = reader.GetInt32(0),
+                            PermissionName = reader.GetString(1),
+                            Resource = reader.GetString(2),
+                            Action = reader.GetString(3)
+                        };
+                    }
+                }
+            }
+            return permission;
+        }
+
+        public List<Permission> GetPermissions()
+        {
+            List<Permission> permissions = new List<Permission>();
+            string query = "SELECT Id, permissionName, resource, action FROM Permissions";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Permission permission = new Permission
+                        {
+                            Id = reader.GetInt32(0),
+                            PermissionName = reader.GetString(1),
+                            Resource = reader.GetString(2),
+                            Action = reader.GetString(3)
+                        };
+                        permissions.Add(permission);
+                    }
+                }
+            }
+            return permissions;
+        }
     }
 }
--- Adapter/ResourceAdapter.cs
+++ Adapter/ResourceAdapter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Configuration;
 using DrinkDb_Auth.Adapter;
 using DrinkDb_Auth.Database;
@@ -17,11 +17,94 @@ namespace DrinkDb_Auth.Database
             connectionString = ConfigurationManager.ConnectionStrings["DrinkDbConnection"].ConnectionString;
         }
 
-        SqlConnection conn = new SqlConnection();
-        SqlCommand cmd = new SqlCommand();
+        public void CreateResource(Resource resource)
+        {
+            string query = "INSERT INTO Resources (Name, Description) VALUES (@Name, @Description)";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Name", resource.Name);
+                cmd.Parameters.AddWithValue("@Description", resource.Description);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
 
+        public void UpdateResource(Resource resource)
+        {
+            string query = "UPDATE Resources SET Name=@Name, Description=@Description WHERE Id=@Id";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Name", resource.Name);
+                cmd.Parameters.AddWithValue("@Description", resource.Description);
+                cmd.Parameters.AddWithValue("@Id", resource.Id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
 
+        public void DeleteResource(Resource resource)
+        {
+            string query = "DELETE FROM Resources WHERE Id=@Id";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Id", resource.Id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
 
+        public Resource GetResourceById(int id)
+        {
+            Resource resource = null;
+            string query = "SELECT Id, Name, Description FROM Resources WHERE Id=@Id";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Id", id);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        resource = new Resource
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Description = reader.GetString(2)
+                        };
+                    }
+                }
+            }
+            return resource;
+        }
+
+        public List<Resource> GetResources()
+        {
+            List<Resource> resources = new List<Resource>();
+            string query = "SELECT Id, Name, Description FROM Resources";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Resource resource = new Resource
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Description = reader.GetString(2)
+                        };
+                        resources.Add(resource);
+                    }
+                }
+            }
+            return resources;
+        }
 
     }
 }
