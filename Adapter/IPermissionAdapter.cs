e947fddee9586fc18ef8907a97941308b321c3af
Pascu-Victor <me@vpascu.ro>
2025-04-02 02:26:17 -0700

    add interfaces for adapters

---
+++ Adapter/IPermissionAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkDb_Auth.Adapter
+{
+    internal interface IPermissionAdapter
+    {
+        public void CreatePermission(Permission permission);
+        public void UpdatePermission(Permission permission);
+        public void DeletePermission(Permission permission);
+        public Permission GetPermissionById(int id);
+        public List<Permission> GetPermissions();
+    }
+}
---
+++ Adapter/IResourceAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrinkDb_Auth.Models;
+
+namespace DrinkDb_Auth.Adapter
+{
+    internal interface IResourceAdapter
+    {
+        public void CreateResource(Resource resource);
+        public void UpdateResource(Resource resource);
+        public void DeleteResource(Resource resource);
+        public Resource GetResourceById(int id);
+        public List<Resource> GetResources();
+    }
+}
