721ee8c54b3cea08d3e294ebb22bb652239a37e9
VioreliaMotofelea <viorelia.motofelea123@gmail.com>
2025-04-02 12:29:11 +0300

    without model

--- Adapter/IResourceAdapter.cs
+++ Adapter/IResourceAdapter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DrinkDb_Auth.Models;
 
 namespace DrinkDb_Auth.Adapter
 {
---
+++ Adapter/Resources.cs
@@ -0,0 +1,9 @@
+﻿namespace DrinkDb_Auth.Adapter
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
