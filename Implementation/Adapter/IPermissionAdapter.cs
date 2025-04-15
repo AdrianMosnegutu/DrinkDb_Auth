aa6e54aeff972ae4affd0d6643a813e52fefeb50
AndreiCatrinescu <andrei.catrinescu.04@gmail.com>
2025-04-15 14:14:22 +0300

    :((((((((((((((((

--- Implementation/Adapter/IPermissionAdapter.cs
+++ Implementation/Adapter/IPermissionAdapter.cs
@@ -12,7 +12,7 @@ namespace DrinkDb_Auth.Adapter
         public void CreatePermission(Permission permission);
         public void UpdatePermission(Permission permission);
         public void DeletePermission(Permission permission);
-        public Permission GetPermissionById(int id);
+        public Permission GetPermissionById(Guid id);
         public List<Permission> GetPermissions();
     }
 }
